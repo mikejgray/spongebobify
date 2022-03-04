9cad8decf579bafc94353a03f04528192a2bfb6d
Mike Gray <mike@graywind.org>
2022-03-03 19:49:44 -0600

    Add unit tests

---
+++ tests/test_spongebobify.py
@@ -0,0 +1,10 @@
+from spongebobify.main import aye_aye
+
+def test_aye_aye_non_alpha_no_change():
+    assert aye_aye("111111!") == "111111!"
+
+def test_aye_aye_text_not_altered():
+    assert aye_aye("Are ya ready kids").lower() == "Are ya ready kids".lower()
+    
+def test_aye_aye_unicode_symbols():
+    assert aye_aye("˙˙∂∂∂∂∂˚¥∑∑®") == "˙˙∂∂∂∂∂˚¥∑∑®"
